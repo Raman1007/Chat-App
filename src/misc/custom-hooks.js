730190bb7517bd30880c99b620f385617b0a01e2
Raman <ramansonker900@gmail.com>
2021-07-21 18:43:21 +0530

    added programatical hover

--- src/misc/custom-hooks.js
+++ src/misc/custom-hooks.js
@@ -48,16 +48,20 @@ export function useHover() {
   const ref = useRef(null);
   const handleMouseOver = () => setValue(true);
   const handleMouseOut = () => setValue(false);
-  useEffect(() => {
-    const node = ref.current;
-    if (node) {
-      node.addEventListener('mouseover', handleMouseOver);
-      node.addEventListener('mouseout', handleMouseOut);
+  useEffect(
+    () => {
+      const node = ref.current;
+      if (node) {
+        node.addEventListener('mouseover', handleMouseOver);
+        node.addEventListener('mouseout', handleMouseOut);
+      }
       return () => {
         node.removeEventListener('mouseover', handleMouseOver);
         node.removeEventListener('mouseout', handleMouseOut);
       };
-    }
-  }, []);
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [ref.current] // Recall only if ref changes
+  );
   return [ref, value];
 }
