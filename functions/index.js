1f2c9ca5b401ee64db40cdb1a0f1fd0570cbed7a
Raman <ramansonker900@gmail.com>
2021-07-26 11:22:20 +0530

    added cloud function to send messages

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,3 @@
-/* eslint-disable quotes */
-// eslint-disable-next-line no-unused-vars
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
@@ -10,10 +8,5 @@ admin.initializeApp({
   databaseURL: 'https://chat-web-app-3b298-default-rtdb.firebaseio.com',
 });
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const { sendFcm } = require('./src/fcm');
+exports.sendFcm = sendFcm;
